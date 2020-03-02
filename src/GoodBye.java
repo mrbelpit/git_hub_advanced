5313b1fdd6835b53ae60b437e35b0628574cd496
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2020-03-02 17:07:35 +0100

    E5 something

--- src/GoodBye.java
+++ src/GoodBye.java
@@ -1,6 +1,6 @@
 public class GoodBye {
 
-    public void goodBye(String name) {
-        System.out.println("Greeting my Fair Lady / Dear Sir "+ name + "!");
+    public void goodBye(String name, String position) {
+        System.out.println("Greeting my Fair Lady / Dear Sir "+ name + "of " + position + "!");
     }
 }
