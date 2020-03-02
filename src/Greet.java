4bec7d2cb037460d62b775a9fbd52abaf1495c38
alaposi <agnes.laposi@gmail.com>
2020-03-02 17:05:41 +0100

    a5

--- src/Greet.java
+++ src/Greet.java
@@ -1,5 +1,5 @@
 public class Greet {
-  public void greet (String name){
-    System.out.println("Hello" + name);
+  public void greet (String greeting, String name){
+    System.out.println(greeting + name);
   }
 }
